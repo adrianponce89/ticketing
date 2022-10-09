e53eb4c1060481cd8686be57c069035f55567d22
Adrian Ponce <adrian.ponce.89@gmail.com>
2022-10-09 20:53:46 -0300

    Create tickets on db

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -1,5 +1,6 @@
 import request from "supertest";
 import { app } from "../../app";
+import { Ticket } from "../../models/ticket";
 
 it("has a route handler listening to /api/tickets fro post request", async () => {
   const response = await request(app).post("/api/tickets").send({});
@@ -61,4 +62,26 @@ it("return an error if invalid price is provided", async () => {
     .expect(400);
 });
 
-it("creates a ticket with valid inputs", async () => {});
+it("creates a ticket with valid inputs", async () => {
+  let tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(0);
+
+  const cookie = await global.signin();
+  const title = "asdf";
+  const price = 10;
+
+  await request(app)
+    .post("/api/tickets")
+    .set("Cookie", cookie)
+    .send({
+      title,
+      price,
+    })
+    .expect(201);
+
+  tickets = await Ticket.find({});
+  expect(tickets.length).toEqual(1);
+
+  expect(tickets[0].title).toEqual(title);
+  expect(tickets[0].price).toEqual(price);
+});
--- tickets/src/routes/new.ts
+++ tickets/src/routes/new.ts
@@ -1,6 +1,7 @@
 import express, { Request, Response } from "express";
 import { body } from "express-validator";
 import { requireAuth, validateRequest } from "@apticketing/common";
+import { Ticket } from "../models/ticket";
 
 const router = express.Router();
 
@@ -15,7 +16,17 @@ router.post(
   ],
   validateRequest,
   async (req: Request, res: Response) => {
-    res.status(200).send({});
+    const { title, price } = req.body;
+
+    const ticket = Ticket.build({
+      title,
+      price,
+      userId: req.currentUser!.id,
+    });
+
+    await ticket.save();
+
+    res.status(201).send(ticket);
   }
 );
 
