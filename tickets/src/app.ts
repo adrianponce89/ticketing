956ea39a3f95e0b9c66ed1e6136b629d1961a675
Adrian Ponce <adrian.ponce.89@gmail.com>
2022-10-13 21:19:48 -0300

    Add get tickets

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -5,6 +5,7 @@ import { errorHandler, NotFoundError, currentUser } from "@apticketing/common";
 
 import { createTicketRouter } from "./routes/new";
 import { showTicketRouter } from "./routes/show";
+import { indexTicketRouter } from "./routes";
 
 const app = express();
 app.set("trust proxy", true);
@@ -20,6 +21,7 @@ app.use(currentUser);
 
 app.use(createTicketRouter);
 app.use(showTicketRouter);
+app.use(indexTicketRouter);
 
 app.all("*", () => {
   throw new NotFoundError();
